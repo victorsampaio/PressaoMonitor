f6027fbfd0f4d76947cc1bb5da9674e50862482e
Victor Sampaio <prolinsprojects@gmail.com>
2017-04-09 18:26:04 -0300

    Commit Inicial
    Commit para iniciar o projeto android de acompanhamento e monitoramento
    de Pressão Corporal

---
+++ PressaoMonitor/app/src/main/java/vs/com/br/pressaomonitor/SplashScreenActivity.java
@@ -0,0 +1,13 @@
+package vs.com.br.pressaomonitor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
