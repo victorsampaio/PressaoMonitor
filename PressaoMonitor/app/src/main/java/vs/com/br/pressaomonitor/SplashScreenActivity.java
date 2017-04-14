4ca9624146e80a944107c9fc4977cde8b2b3e763
Victor Sampaio <prolinsprojects@gmail.com>
2017-04-14 13:18:13 -0300

    Develop branch
    Develop branch

--- PressaoMonitor/app/src/main/java/vs/com/br/pressaomonitor/SplashScreenActivity.java
+++ PressaoMonitor/app/src/main/java/vs/com/br/pressaomonitor/SplashScreenActivity.java
@@ -9,5 +9,8 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+
+
     }
 }
