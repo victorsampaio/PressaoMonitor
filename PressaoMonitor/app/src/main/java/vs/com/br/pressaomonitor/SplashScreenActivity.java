f3e1afe435958c1a0cf9995d7b626a8f902284ab
Victor Sampaio <prolinsprojects@gmail.com>
2017-04-14 14:16:55 -0300

    Task #1 - Criação da SplashScreen

--- PressaoMonitor/app/src/main/java/vs/com/br/pressaomonitor/SplashScreenActivity.java
+++ PressaoMonitor/app/src/main/java/vs/com/br/pressaomonitor/SplashScreenActivity.java
@@ -1,16 +1,40 @@
 package vs.com.br.pressaomonitor;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class SplashScreenActivity extends AppCompatActivity {
+import vs.com.br.pressaomonitor.view.RegisterArterialPressureActivity;
+
+public class SplashScreenActivity extends AppCompatActivity implements Runnable {
+    private static final String Debug = " Debug: ";
+    private static final String TAG = "Basic Network Demo";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
+        Handler action = new Handler();
+        int DELAY = 5000;
+        action.postDelayed(this, DELAY);
 
+    }
 
+    @Override
+    public void run() {
+        Log.d(Debug, "Method - run() ");
+        //checkNetworkConnection();
+        startApplication();
+    }
+
+    private void startApplication() {
+        Log.d(Debug, "Method - startLogin() ");
+        Intent intent = new Intent(this, RegisterArterialPressureActivity.class);
+        intent.putExtra("message", "Splash");
+        startActivity(intent);
+        finish();
     }
 }
